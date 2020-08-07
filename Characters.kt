a0ae72dda56799297c7ff314ea27f5a51cbbf215
MichaelAgustinusS <68942897+MichaelAgustinusS@users.noreply.github.com>
2020-08-07 17:51:16 +0700

    Add files via upload

---
+++ Characters.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var vocal = 'Z'
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    vocal--
+    vocal--
+    vocal--
+    vocal--
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    vocal++
+    vocal++
+    vocal++
+    vocal++
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+}
---
+++ IfExpressions.kt
@@ -0,0 +1,12 @@
+fun main () {
+    val kkm = 75
+    val nilaiuh = 80
+    val Nilai: String
+    if (nilaiuh > kkm) {
+        Nilai = "Nilai kamu " + nilaiuh+ ", selamat ya"
+    } else {
+        Nilai = "Nilau kamu " + nilaiuh+ ", silahkan ikuti remidial"
+    }
+
+    print(Nilai)
+}
