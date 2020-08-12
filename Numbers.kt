418a847b956b29218d4b628d866881961d8716ec
MichaelAgustinusS <68942897+MichaelAgustinusS@users.noreply.github.com>
2020-08-12 22:29:48 +0700

    Add files via upload

---
+++ Numbers.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val p = 22
+    val l = 12.6
+    val t = 11
+    val volume : Double
+}
+fun hitungVolume(p : Double, l : Double, t : Double) {
+    val v = p * l * t
+print("Volume dari balok yang mempunyai panjang $p, lebar $l, dan tinggi $t ini adalah $v")
+}
